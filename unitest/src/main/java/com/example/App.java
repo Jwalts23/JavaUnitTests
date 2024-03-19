fe40e8101cd0b2d48cd6876d68f24560942c8ba4
Jolie Walts <81712807+Jwalts23@users.noreply.github.com>
2024-03-19 12:02:41 -0400

    More fucntions and unit tests

--- unitest/src/main/java/com/example/App.java
+++ unitest/src/main/java/com/example/App.java
@@ -6,19 +6,60 @@ package com.example;
  */
 public class App 
 {
+    static String outputString = "";
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(calculateArea(5));
-        sum(400.3, 800.2);
+        System.out.println(calculateAreaCircle(5));
+        add(400.3, 800.2);
     }
 
-    public static double calculateArea(double radius) {
+    public static double calculateAreaCircle(double radius) {
         return Math.PI * radius * radius;
     }
-    public static double sum(double n1, double n2){
+
+    public static double calculateAreaSquare(double r) {
+        return r * r;
+    }
+
+    public static double calculateAreaRectangle(double b, double h) {
+        return b * h;
+    }
+
+
+    public static double calculateAreaTrapizoid(double a, double b, double h) {
+        return (a + b) * .5 * h;
+    }
+
+    public static double add(double n1, double n2){
         double sum = n1 + n2;
-        System.out.println("The sum of " + n1 + " + " + n2 + " is: " + sum);
+        outputString = "The sum of " + n1 + " + " + n2 + " is: " + sum; 
+        print(outputString);
         return sum;
     }
+    public static double subtract(double n1, double n2){
+        double sum = n1 - n2;
+        System.out.println("The sum of " + n1 + " - " + n2 + " is: " + sum);
+        print(outputString);
+        return sum;
+    }
+
+    public static double divide(double n1, double n2){
+        double div = n1/n2;
+        outputString = "The result of " + n1 + "/" + n2 + " is: " + div;
+        print(outputString);
+        return div;
+    }
+
+    public static double multiply(double n1, double n2){
+        double div = n1*n2;
+        outputString = "The result of " + n1 + "*" + n2 + " is: " + div;
+        print(outputString);
+        return div;
+    }
+
+    public static void print(String msg){
+        System.out.println(msg);
+    }
+
 }
--- unitest/src/test/java/com/example/AppTest.java
+++ unitest/src/test/java/com/example/AppTest.java
@@ -6,24 +6,79 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 
     @Test
-    public void testCalculateArea(){
-        assertEquals(3.14159265, App.calculateArea(1d), 0.000001);
+    public void testCalculateArea() {
+        assertEquals(3.14159265, App.calculateAreaCircle(1d), 0.000001);
+    }
+
+
+
+    @Test
+    public void testSum() {
+        assertEquals(1200.5, App.add(400.3, 800.2), 0.001);
+    }
+
+
+    @Test
+    public void TestOutputStringSum(){
+        assertEquals("The sum of 400.3 + 800.2 is: 1200.5", App.outputString);
     }
 
     @Test
-    public void testSum(){
-        assertEquals(1200.5, App.sum(400.3, 800.2), 0);
+    public void testSum2() {
+        assertEquals(-1200.5, App.add(-400.3, -800.2), 0.001);
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(400.4, App.subtract(800.6, 400.2), 0.001);
+    }
+
+    @Test
+    public void testSubtract2() {
+        assertEquals(-1.6, App.subtract(800.6, 802.2), 0.001);
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, App.divide(4, 2), 0.001);
+    }
+
+    @Test
+    public void testDivide2() {
+        assertEquals(.5, App.divide(2, 4), 0.001);
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(8, App.multiply(4, 2), 0.001);
+    }
+
+    @Test
+    public void testMultiply2() {
+        assertEquals(2, App.multiply(4, .5), 0.001);
+    }
+
+    @Test
+    public void orderOfOperations() {
+        double val = App.multiply(2, (App.add(2, 6) + App.add(1, 2)));
+        assertEquals(22, val, 0.001);
+
+    }
+
+    @Test
+    public void orderOfOperations2() {
+        double val = App.multiply(2, App.multiply(App.add(2, 6), App.add(1, 2)));
+        assertEquals(48, val, 0.001);
+
     }
 }
